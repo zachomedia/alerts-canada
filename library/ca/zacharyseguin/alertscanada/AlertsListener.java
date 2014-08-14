668c20b806ba15656434b4d8c75573c2380eeb67
Zachary Seguin <development@zacharyseguin.ca>
2014-08-13 21:40:59 -0400

    Some initial work

---
+++ library/ca/zacharyseguin/alertscanada/AlertsListener.java
@@ -0,0 +1,63 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Zachary Seguin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package ca.zacharyseguin.alertscanada;
+
+/**
+ * AlertsListener is an interface that can be implemented to
+ * receive new, updated and ended alerts.
+ *
+ * @author      Zachary Seguin
+ * @version     1.0
+ * @since       1.0
+ */
+public interface AlertsListener
+{
+    /**
+     * A new alert has been received.
+     *
+     * @param alert The alert that was received.
+     *
+     * @since 1.0
+     */
+    public void alertReceived(Alert alert);
+
+    /**
+     * An existing alert has been updated.
+     *
+     * @param newAlert The updated version of the alert.
+     * @param oldAlert The previous version of the alert.
+     *
+     * @since 1.0
+     */
+    public void alertUpdated(Alert newAlert, Alert oldAlert);
+
+    /**
+     * An alert has ended.
+     *
+     * @param alert The alert that has ended.
+     *
+     * @since 1.0
+     */
+    public void alertEnded(Alert alert);
+}// End of interface
