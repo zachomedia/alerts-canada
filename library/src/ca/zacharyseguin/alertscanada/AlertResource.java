de1ba619572e5ce37948ea2e67db9e7572b2befa
Zachary Seguin <development@zacharyseguin.ca>
2014-08-17 13:16:37 -0400

    Added getters to AlertResource

--- library/src/ca/zacharyseguin/alertscanada/AlertResource.java
+++ library/src/ca/zacharyseguin/alertscanada/AlertResource.java
@@ -35,8 +35,14 @@ import java.net.URL;
  * @version     1.0
  * @since       1.0
  */
-public class AlertResource implements Serializable
+public class AlertResource implements Cloneable, Serializable
 {
+    /**
+     * Size was not provided.
+     * @since 1.0
+     */
+    public static int SIZE_NOT_PROVIDED = -1;
+
     /**
      * Text describing the type and contents of the resource file.
      * @since 1.0
@@ -73,4 +79,78 @@ public class AlertResource implements Serializable
      * @since 1.0
      */
     private String digest;
+
+    /**
+     * Default constructor - Construct resource with AlertResources.Builder.
+     * @since 1.0
+     */
+    private AlertResource()
+    {
+        this.description = null;
+        this.mimeType = null;
+        this.size = SIZE_NOT_PROVIDED;
+        this.uri = null;
+        this.derefUri = null;
+        this.digest = null;
+    }// End of constructor method
+
+    /**
+     * Returns the description of the resource.
+     * @return Description of the resource.
+     * @since 1.0
+     */
+    public String getDescription()
+    {
+        return this.description;
+    }// End of getDescription method
+
+    /**
+     * Returns the mime type of the resource.
+     * @return Mime type of the resource.
+     * @since 1.0
+     */
+    public String getMimeType()
+    {
+        return this.mimeType;
+    }// End of getMimeType method
+
+    /**
+     * Returns the size of the resource.
+     * @return Size of the resource. Returns AlertResource.SIZE_NOT_PROVIDED when size is not provided.
+     * @since 1.0
+     */
+    public int getSize()
+    {
+        return this.size;
+    }// End of getSize method
+
+    /**
+     * Retruns the hyperlink to the resource.
+     * @return Hyperlink to the resource.
+     * @since 1.0
+     */
+    public URL getUri()
+    {
+        return this.uri;
+    }// End of getUri method
+
+    /**
+     * Returns the BASE64 encoded resource.
+     * @return BASE64 encoded resource.
+     * @since 1.0
+     */
+    public String getDerefUri()
+    {
+        return this.derefUri;
+    }// End of getDerefUri method
+
+    /**
+     * Returns the digital digest (hash) of the resoruce.
+     * @return Digital digest (hash) of the resource.
+     * @since 1.0
+     */
+    public String getDigest()
+    {
+        return this.digest;
+    }// End of getDigest method
 }// End of class
